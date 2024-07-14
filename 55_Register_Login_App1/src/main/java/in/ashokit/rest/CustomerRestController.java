735a749b6e7b20870203ce68ee9f36f412127bb1
Sahil <sahilansari567@gmail.com>
2024-07-15 00:06:29 +0530

    first commit

---
+++ 55_Register_Login_App1/src/main/java/in/ashokit/rest/CustomerRestController.java
@@ -0,0 +1,53 @@
+package in.ashokit.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.ashokit.entity.Customer;
+import in.ashokit.repo.CustomerRepo;
+
+@RestController
+public class CustomerRestController {
+	@Autowired
+	private CustomerRepo repo;
+	@Autowired
+	private PasswordEncoder pwdEncoder;
+	@Autowired
+	private AuthenticationManager authManager;
+
+	@PostMapping("/login")
+	public ResponseEntity<String> loginCheck(@RequestBody Customer c) {
+
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(c.getEmail(), c.getPwd());
+
+		try {
+
+			Authentication authenticate = authManager.authenticate(token);
+			if (authenticate.isAuthenticated()) {
+				return new ResponseEntity<String>("welcome to Login page", HttpStatus.OK);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<String>("invalid crediantials", HttpStatus.UNAUTHORIZED);
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<String> registration(@RequestBody Customer c) {
+
+		String encodedPwd = pwdEncoder.encode(c.getPwd());
+		c.setPwd(encodedPwd);
+		repo.save(c);
+		return new ResponseEntity<String>("User Registered", HttpStatus.CREATED);
+	}
+}
